fe589dd7f9bf37e000541b196b72bc1624256b4c
Maximilian Eschenbacher <maximilian@eschenbacher.email>
2019-02-17 19:13:38 +0100

    use BORG_REPO env variable

--- borg/files/borgbackup.sh
+++ borg/files/borgbackup.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-readonly REPO={{salt.pillar.get('borg:archive_base')}}{{salt.pillar.get('borg:reponame', '$HOSTNAME')}}
-
+export BORG_REPO={{salt.pillar.get('borg:archive_base')}}{{salt.pillar.get('borg:reponame', '$HOSTNAME')}}
 export BORG_RELOCATED_REPO_ACCESS_IS_OK=yes
 export BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK=yes
 export BORG_PASSPHRASE={{salt.pillar.get('borg:passphrase', '')}}
 
-borg create "$REPO"::'{hostname}-{now}' \
+borg create '{hostname}-{now}' \
 	/ \
 	--exclude /bin \
 	--exclude /boot \
--- borg/files/borgcheck.sh
+++ borg/files/borgcheck.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-readonly REPO=ssh://borg@borg.kajt.de:42/srv/borg-repos/{{salt['pillar.get']('borg:reponame', '$HOSTNAME')}}
-
+export BORG_REPO={{salt.pillar.get('borg:archive_base')}}{{salt.pillar.get('borg:reponame', '$HOSTNAME')}}
 export BORG_RELOCATED_REPO_ACCESS_IS_OK=yes
 export BORG_PASSPHRASE={{salt['pillar.get']('borg:passphrase', '')}}
 
-borg check $REPO --last 7 || exit $?
+borg check :: --last 7 || exit $?
 
 exit $?
